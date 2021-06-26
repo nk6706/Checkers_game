edd3d9fce199037e602db38ae65fdf445cc1c551
AnthonyMSalvati <ams6995@rit.edu>
2021-06-25 20:21:58 -0400

    Created POST route, still WIP

---
+++ src/main/java/com/webcheckers/ui/PostSignInRoute.java
@@ -0,0 +1,33 @@
+package com.webcheckers.ui;
+
+import com.webcheckers.Model.Player;
+import com.webcheckers.Model.PlayerLobby;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+
+public class PostSignInRoute implements Route {
+
+    static final String USERNAME_PARAM = "myUsername";
+    static final String VIEW_NAME = "sign-in.ftl";
+    private PlayerLobby playerLobby;
+
+    PostSignInRoute(PlayerLobby playerLobby){
+        this.playerLobby = playerLobby;
+    }
+
+    @Override
+    public Object handle(Request request, Response response) throws Exception {
+        final String username = request.queryParams(USERNAME_PARAM);
+        Player newPlayer = new Player(username);
+
+        if (playerLobby.getPlayerList().contains(newPlayer)){
+            //error message here
+        }
+        else {
+            playerLobby.addPlayer(newPlayer);
+        }
+
+        return null;
+    }
+}
