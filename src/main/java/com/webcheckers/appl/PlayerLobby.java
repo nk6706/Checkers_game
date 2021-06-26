73031398cad7ca62e464fd1d213e68beda44c93c
William Tietjen <wft4166@rit.edu>
2021-06-26 17:31:28 -0400

    changed playerList to hashmap for easier lookup

--- src/main/java/com/webcheckers/appl/PlayerLobby.java
+++ src/main/java/com/webcheckers/appl/PlayerLobby.java
@@ -6,26 +6,30 @@ import java.util.*;
 
 public class PlayerLobby {
 
-    private List<Player> playerList;
+    private HashMap<String, Player> playerList;
 
     public PlayerLobby(){
-
-    }
-
-    public PlayerLobby(List<Player> playerList){
-
-        this.playerList = playerList;
+        this.playerList = new HashMap<String, Player>();
     }
 
     public void addPlayer(Player player){
-        playerList.add(player);
+        playerList.put(player.getUsername(), player);
     }
 
     public int getNumberOfPlayers() {
         return playerList.size();
     }
 
-    public List<Player> getPlayerList(){
+    /**
+     * Check if the given player (username) exists in the lobby
+     * @param username the name of the player to check for
+     * @return true if the player is present, false otherwise
+     */
+    public boolean hasPlayer(String username) {
+        return this.playerList.containsKey(username);
+    }
+
+    public HashMap<String, Player> getPlayerList(){
         return playerList;
     }
 
