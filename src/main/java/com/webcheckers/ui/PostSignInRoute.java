1e95e7da9cf7ce649827dabe924e90ef2d659c53
William Tietjen <wft4166@rit.edu>
2021-06-26 20:05:54 -0400

    added checks to make sure username meets criteria, redirects back to signin with error msg if anything comes up

--- src/main/java/com/webcheckers/ui/PostSignInRoute.java
+++ src/main/java/com/webcheckers/ui/PostSignInRoute.java
@@ -5,27 +5,41 @@ import com.webcheckers.appl.PlayerLobby;
 import spark.Request;
 import spark.Response;
 import spark.Route;
+import spark.Session;
+
+import java.util.logging.Logger;
 
 public class PostSignInRoute implements Route {
+    private static final Logger LOG = Logger.getLogger(GetSignInRoute.class.getName());
 
-    static final String USERNAME_PARAM = "myUsername";
-    static final String VIEW_NAME = "sign-in.ftl";
-    private PlayerLobby playerLobby;
+    static final String USERNAME_PARAM = "username";
+    static final String VIEW_NAME = "signin.ftl";
+    private final PlayerLobby playerLobby;
 
-    PostSignInRoute(PlayerLobby playerLobby){
+    public PostSignInRoute(PlayerLobby playerLobby){
         this.playerLobby = playerLobby;
+        //
+        LOG.config("PostSignInRoute is initialized.");
     }
 
     @Override
     public Object handle(Request request, Response response) throws Exception {
         final String username = request.queryParams(USERNAME_PARAM);
-        Player newPlayer = new Player(username);
+        final Session httpSession = request.session();
 
-        if (playerLobby.getPlayerList().contains(newPlayer)){
-            //error message here
-        }
-        else {
-            playerLobby.addPlayer(newPlayer);
+        // Check that username is at least one alphanumeric character and contains no symbol
+        if (username.length() > 0 && username.chars().allMatch( c -> Character.isLetterOrDigit(c) || Character.isWhitespace(c)) && username.chars().anyMatch(Character::isLetterOrDigit)) {
+            if (!this.playerLobby.hasPlayer(username)) { // Check that username does not already exist
+                final Player player = new Player(username);
+                httpSession.attribute("player", player);
+                playerLobby.addPlayer(player);
+                response.redirect(WebServer.HOME_URL);
+            }
+            else {
+                response.redirect(WebServer.SIGN_IN_URL + "?error=Name taken");
+            }
+        } else {
+            response.redirect(WebServer.SIGN_IN_URL + "?error=Username must contain at least one alphanumeric character");
         }
 
         return null;
