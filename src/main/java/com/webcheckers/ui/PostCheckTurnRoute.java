5cf9dc72e818f684bec1f5e66372e03691e22e41
William Tietjen <wft4166@rit.edu>
2021-07-07 20:16:38 -0400

    began implementation of handle method

--- src/main/java/com/webcheckers/ui/PostCheckTurnRoute.java
+++ src/main/java/com/webcheckers/ui/PostCheckTurnRoute.java
@@ -1,5 +1,9 @@
 package com.webcheckers.ui;
 
+import com.webcheckers.appl.GameManager;
+import com.webcheckers.model.CheckersGame;
+import com.webcheckers.model.Player;
+import com.webcheckers.util.Message;
 import spark.Request;
 import spark.Response;
 import spark.Route;
@@ -10,14 +14,26 @@ import java.util.logging.Logger;
 public class PostCheckTurnRoute implements Route {
     private static final Logger LOG = Logger.getLogger(PostCheckTurnRoute.class.getName());
 
-    public PostCheckTurnRoute() {
+    private final GameManager gameManager;
+
+    public PostCheckTurnRoute(GameManager gameManager) {
+        this.gameManager = gameManager;
         //
         LOG.config("PostCheckTurnRoute is initialized.");
     }
 
     @Override
     public Object handle(Request request, Response response) throws Exception {
+        final Session httpSession = request.session();
+        final Player player = httpSession.attribute("player");
 
-        return null;
+        CheckersGame game = gameManager.getGame(player.getGameID());
+        Message result = Message.info("false");
+        if (game.isPlayersTurn(player)) {
+            result = Message.info("true");
+        }
+
+        result = Message.info("true");
+        return result;
     }
 }
