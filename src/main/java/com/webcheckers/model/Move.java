719fa000f87704edb4b6bfc636bc4e04220b1c33
AnthonyMSalvati <ams6995@rit.edu>
2021-07-08 22:26:35 -0400

    Beginning move verification

---
+++ src/main/java/com/webcheckers/model/Move.java
@@ -0,0 +1,41 @@
+package com.webcheckers.model;
+
+public class Move {
+
+    private Position startingPosition;
+    private Position endingPosition;
+    private CheckerPiece currentPiece;
+
+
+    public Move(Position start, Position end, CheckerPiece pieceToMove){
+        this.startingPosition = start;
+        this.endingPosition = end;
+        currentPiece = pieceToMove;
+    }
+
+    public boolean isValid(CheckerBoard board){
+
+        // is the move within board constraints?
+        if (endingPosition.getX() < 0 || endingPosition.getX() > 7){
+            return false;
+        } else if (endingPosition.getY() < 0 || endingPosition.getY() > 7){
+            return false;
+        }
+
+        // is the desired space empty?
+        if (!(board.getBoard()[endingPosition.getX()][endingPosition.getY()] == null)){
+            return false;
+        }
+
+        // is the piece moving in a valid direction?
+        if (!(this.currentPiece.getType() == CheckerPiece.Type.KING)){
+            if (this.currentPiece.getColor() == CheckerPiece.Color.WHITE && endingPosition.getY() > startingPosition.getY()){
+                return false;
+            } else if (this.currentPiece.getColor() == CheckerPiece.Color.RED && endingPosition.getY() < startingPosition.getY()){
+                return  false;
+            }
+        }
+
+        return true;
+    }
+}
