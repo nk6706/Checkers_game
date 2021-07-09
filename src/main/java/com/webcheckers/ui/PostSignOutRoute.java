73e7e8593183bdc12a92b68a967da2d65a025740
Trevor Cruz <tdc8319@rit.edu>
2021-07-09 02:24:54 -0400

    Fully implemented the PostSignOutRoute and the signout button now works.

--- src/main/java/com/webcheckers/ui/PostSignOutRoute.java
+++ src/main/java/com/webcheckers/ui/PostSignOutRoute.java
@@ -1,12 +1,40 @@
 package com.webcheckers.ui;
 
+import com.webcheckers.appl.PlayerLobby;
+import com.webcheckers.model.Player;
 import spark.Request;
 import spark.Response;
 import spark.Route;
+import spark.Session;
+import spark.*;
+
+import java.util.logging.Logger;
 
 public class PostSignOutRoute implements Route {
+    private static final Logger LOG = Logger.getLogger(PostSignOutRoute.class.getName());
+
+    private final PlayerLobby playerLobby;
+
+    public PostSignOutRoute(PlayerLobby playerLobby) {
+        this.playerLobby = playerLobby;
+        LOG.config("PostSignOutRoute is initialized.");
+    }
+
+    //TODO:
+    //  1. Delete player from PlayerLobby
+    //  2. Remove the player attribute from the session and set it to null
+    //  3. Redirect back to the home page with GET "/"
     @Override
     public Object handle(Request request, Response response) throws Exception {
-        return null;
+        final Session httpSession = request.session();
+        final Player playerName = (Player) httpSession.attribute("player");
+
+        this.playerLobby.playerSignOut(playerName.getUsername());
+
+        httpSession.removeAttribute("player");
+
+        response.redirect(WebServer.HOME_URL);
+
+        return "";
     }
 }
