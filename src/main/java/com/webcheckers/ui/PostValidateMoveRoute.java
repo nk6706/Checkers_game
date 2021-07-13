cc892fb4fba5da99174ef7e73942b10e4055dd77
William Tietjen <wft4166@rit.edu>
2021-07-13 10:46:14 -0400

    stubbed out PostValidateMoveRoute

---
+++ src/main/java/com/webcheckers/ui/PostValidateMoveRoute.java
@@ -0,0 +1,36 @@
+package com.webcheckers.ui;
+
+import com.google.gson.Gson;
+import com.webcheckers.appl.GameManager;
+import com.webcheckers.model.CheckersGame;
+import com.webcheckers.model.Player;
+import com.webcheckers.util.Message;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Session;
+
+import java.util.logging.Logger;
+
+public class PostValidateMoveRoute implements Route {
+    private static final Logger LOG = Logger.getLogger(PostValidateMoveRoute.class.getName());
+
+    private final GameManager gameManager;
+    private final Gson gson;
+
+    public PostValidateMoveRoute(GameManager gameManager, Gson gson) {
+        this.gameManager = gameManager;
+        this.gson = gson;
+        //
+        LOG.config("PostValidateMoveRoute is initialized.");
+    }
+
+    @Override
+    public Object handle(Request request, Response response) throws Exception {
+        final Session httpSession = request.session();
+        final Player player = httpSession.attribute("player");
+
+
+        return null;
+    }
+}
