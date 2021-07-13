a794925ea77cfd6557fd482f6739af7cd8b7ddaf
Kyle Reddy <kbr3836@rit.edu>
2021-07-13 01:25:54 -0400

    added single space move validation

--- src/main/java/com/webcheckers/model/Move.java
+++ src/main/java/com/webcheckers/model/Move.java
@@ -9,11 +9,13 @@ public class Move {
     private Position startingPosition;
     private Position endingPosition;
     private CheckerPiece[][] currentBoard;
+    private CheckerPiece.Color playerColor;
 
-    public Move(Position start, Position end, CheckerPiece[][] currentBoard){
+    public Move(Position start, Position end, CheckerPiece[][] currentBoard, CheckerPiece.Color playerColor){
         this.startingPosition = start;
         this.endingPosition = end;
         this.currentBoard = currentBoard;
+        this.playerColor = playerColor;
     }
 
     public MoveValidity isValid(){
@@ -24,11 +26,8 @@ public class Move {
         int endPosY = endingPosition.getY();
 
         // is the move within board constraints?
-        if (endPosX < 0 || endPosX > 7 || startPosX < 0 || startPosX > 7){
-            return MoveValidity.INVALID;
-        } else if (endPosY < 0 || endPosY > 7 || startPosY < 0 || startPosY > 7){
-            return MoveValidity.INVALID;
-        }
+        if (endPosX < 0 || endPosX > 7 || startPosX < 0 || startPosX > 7) return MoveValidity.INVALID;
+        else if (endPosY < 0 || endPosY > 7 || startPosY < 0 || startPosY > 7) return MoveValidity.INVALID;
 
         if (currentBoard[startPosY][startPosX] == null || currentBoard[endPosY][endPosX] != null) {
             return MoveValidity.INVALID;
@@ -36,40 +35,61 @@ public class Move {
 
         CheckerPiece movingPiece = currentBoard[startPosY][startPosX];
 
-        if (movingPiece.getType() != CheckerPiece.Type.KING) {
-            // If single space diagonal move
-            if (endPosY == startPosY - 1 && (endPosX == startPosX - 1 || endPosX == startPosX + 1)) {
+        if (movingPiece.getColor() != playerColor) return MoveValidity.INVALID;
 
+        if (movingPiece.getType() != CheckerPiece.Type.KING) {
+            // If single space move
+            if (endPosY == startPosY - 1 && (endPosX == startPosX - 1 || endPosX == startPosX + 1)) {
+                return singleSpaceMove(startPosX, startPosY, endPosX, endPosY);
             } // If jump move
             else if (endPosY == startPosY - 2 && (endPosX == startPosX - 2 || endPosX == startPosX + 2)) {
-
+                // TODO: add jump move behavior
             }
         }
 
-        /*
-        // is the desired space empty?
-        if (!(board.getBoard()[endingPosition.getX()][endingPosition.getY()] == null)){
-            return false;
-        }*/
-
-        /* TODO: modify for currentBoard variable
-        // is the piece moving in a valid direction?
-        if (!(this.currentPiece.getType() == CheckerPiece.Type.KING)){
-            if (this.currentPiece.getColor() == CheckerPiece.Color.WHITE && endingPosition.getY() > startingPosition.getY()){
-                return false;
-            } else if (this.currentPiece.getColor() == CheckerPiece.Color.RED && endingPosition.getY() < startingPosition.getY()){
-                return  false;
-            }
-        }
-        */
-
-        /*
-        // is the piece moving to a valid square?
-        if (endingPosition.getX()%2 == 0 || endingPosition.getY()%2 == 0){
-            return false;
-        }*/
+        // TODO: add king piece behavior
 
         return MoveValidity.VALID_END_OF_TURN;
+    }
+
+    private MoveValidity singleSpaceMove(int startPosX, int startPosY, int endPosX, int endPosY) {
+        // If moving diagonally left one space
+        if (endPosX == startPosX - 1) {
+            // and 2 spaces diagonally right exists on board
+            if (!(startPosX + 2 > 7) && !(startPosY - 2 < 0)) {
+                // and the space one diagonally to the right contains a piece
+                if (currentBoard[endPosY][startPosX + 1] != null) {
+                    // and that piece is not one of the player's
+                    if (currentBoard[endPosY][startPosX + 1].getColor() != playerColor) {
+                        // and space 2 diagonally to the right is null
+                        if (currentBoard[startPosY - 2][startPosX + 2] == null) {
+                            return MoveValidity.INVALID_JUMP_REQUIRED;
+                        }
+                    }
+                }
+            }
+            return MoveValidity.VALID_END_OF_TURN;
+        }
+        // If moving diagonally right one space
+        else if (endPosX == startPosX + 1) {
+            // and 2 spaces diagonally left exists on board
+            if (!(startPosX - 2 < 0) && !(startPosY - 2 < 0)) {
+                // and the space one diagonally to the left contains a piece
+                if (currentBoard[endPosY][startPosX - 1] != null) {
+                    // and that piece is not one of the player's
+                    if (currentBoard[endPosY][startPosX - 1].getColor() != playerColor) {
+                        // and space 2 diagonally to the left is null
+                        if (currentBoard[startPosY - 2][startPosX - 2] == null) {
+                            return MoveValidity.INVALID_JUMP_REQUIRED;
+                        }
+                    }
+                }
+            }
+            return MoveValidity.VALID_END_OF_TURN;
+
+        } else {
+            return MoveValidity.INVALID;
+        }
     }
 
     public CheckerBoard makeMove(CheckerBoard board){
