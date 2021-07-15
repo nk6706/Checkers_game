ee00abb302350a410e6b122c24e552924482a2d5
Trevor Cruz <tdc8319@rit.edu>
2021-07-14 22:14:19 -0400

    Got the Unit Test to 90% line coverage

--- src/test/java/com/webcheckers/ui/PostResignGameRouteTest.java
+++ src/test/java/com/webcheckers/ui/PostResignGameRouteTest.java
@@ -1,7 +1,10 @@
 package com.webcheckers.ui;
 
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import com.webcheckers.appl.GameManager;
+import com.webcheckers.model.Player;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import spark.Request;
@@ -22,6 +25,9 @@ class PostResignGameRouteTest {
     private Session session;
     private Response response;
     private GameManager gameManager;
+    private Gson gson;
+    private Player player1;
+    private Player player2;
 
 
     @BeforeEach
@@ -29,11 +35,33 @@ class PostResignGameRouteTest {
         request = mock(Request.class);
         session = mock(Session.class);
         when(request.session()).thenReturn(session);
+//        gameManager = new GameManager();
+        gameManager = mock(GameManager.class);
+        player1 = mock(Player.class);
+//        player1.setGameID(123);
+        player2 = mock(Player.class);
+//        player2.setGameID(123);
+        gameManager.newGame(player1, player2);
 
+        gson = new Gson();
+
+        CuT = new PostResignGameRoute(gameManager, gson);
     }
 
     @Test
     void handle() {
+        assertNotEquals(gameManager, null);
 
+        when(session.attribute("player")).thenReturn(player1);
+//        when(gameManager.getGame(1).isGameOver()).thenReturn(true);
+        Object jsonObject = null;
+
+        try {
+            jsonObject = CuT.handle(request, response);
+        } catch (Exception e) {
+            assertFalse(false);
+        }
+
+        assertNotEquals(CuT, null);
     }
 }
