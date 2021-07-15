1f63dc623ee93a5a65c7e7f62993101193068f7f
William Tietjen <wft4166@rit.edu>
2021-07-14 20:16:27 -0400

    added tests

--- src/test/java/com/webcheckers/model/CheckersGameTest.java
+++ src/test/java/com/webcheckers/model/CheckersGameTest.java
@@ -1,12 +1,21 @@
 package com.webcheckers.model;
 
+import com.webcheckers.util.Message;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
+@Tag("Model-tier")
 class CheckersGameTest {
+
+    /** Component-under-Test CuT */
+    private CheckersGame CuT;
+
+    /** Mocks */
     private Player redPlayer;
     private Player whitePlayer;
 
@@ -16,35 +25,80 @@ class CheckersGameTest {
     void setUp() {
         redPlayer = mock(Player.class);
         whitePlayer = mock(Player.class);
+
+        CuT = new CheckersGame(TEST_ID, redPlayer, whitePlayer);
     }
 
     @Test
     public void testGetID() {
-        CheckersGame CuT = new CheckersGame(TEST_ID, redPlayer, whitePlayer);
         assertEquals(TEST_ID, CuT.getId());
     }
 
     @Test
     public void testGetRedPlayer() {
-        CheckersGame CuT = new CheckersGame(TEST_ID, redPlayer, whitePlayer);
         assertEquals(CuT.getRedPlayer(), redPlayer);
     }
 
     @Test
     public void testGetWhitePlayer() {
-        CheckersGame CuT = new CheckersGame(TEST_ID, redPlayer, whitePlayer);
         assertEquals(CuT.getWhitePlayer(), whitePlayer);
     }
 
     @Test
     public void testIsRedPlayerTrue() {
-        CheckersGame CuT = new CheckersGame(1, redPlayer, whitePlayer);
         assertTrue(CuT.isRedPlayer(redPlayer));
     }
 
     @Test
     public void testIsRedPlayerFalse() {
-        CheckersGame CuT = new CheckersGame(1, redPlayer, whitePlayer);
         assertFalse(CuT.isRedPlayer(whitePlayer));
+    }
+
+    @Test
+    public void isValidMoveTest() {
+        Move move = new Move(new Position(5, 4), new Position(4, 5));
+
+        final Message msg = CuT.isValidMove(move);
+
+        assertEquals(Message.Type.INFO, msg.getType());
+        assertEquals("", msg.getText());
+    }
+
+    @Test
+    public void isNotValidMoveTest() {
+        Move move = new Move(new Position(5, 4), new Position(3, 6));
+
+        final Message msg = CuT.isValidMove(move);
+
+        assertEquals(Message.Type.ERROR, msg.getType());
+        assertNotEquals("", msg.getText());
+    }
+
+    @Test
+    public void testIsPlayersTurnTrue() {
+        assertTrue(CuT.isPlayersTurn(redPlayer));
+    }
+
+    @Test
+    public void testIsPlayersTurnFalse() {
+        assertFalse(CuT.isPlayersTurn(whitePlayer));
+    }
+
+    @Test
+    public void testToggleActivePlayer() {
+        CuT.toggleActivePlayer();
+        assertTrue(CuT.isPlayersTurn(whitePlayer));
+    }
+
+    @Test
+    public void isValidTurnTest() {
+        final Message msg = CuT.isValidTurn();
+        assertEquals(Message.Type.INFO, msg.getType());
+        assertEquals("", msg.getText());
+    }
+
+    @Test
+    public void testNewTurn() {
+
     }
 }
