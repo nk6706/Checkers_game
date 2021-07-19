a0137d38b6ca4d33d8c65c537684eecf23e4843d
Trevor Cruz <tdc8319@rit.edu>
2021-07-19 01:24:23 -0400

    Created the route for leaving the spectated game and fully implemented.

--- src/main/java/com/webcheckers/ui/GetSpectatorExitGameRoute.java
+++ src/main/java/com/webcheckers/ui/GetSpectatorExitGameRoute.java
@@ -1,12 +1,40 @@
 package com.webcheckers.ui;
 
+import com.webcheckers.appl.PlayerLobby;
+import com.webcheckers.model.Player;
 import spark.Request;
 import spark.Response;
 import spark.Route;
+import spark.Session;
+
+import java.util.logging.Logger;
 
 public class GetSpectatorExitGameRoute implements Route {
+    private static final Logger LOG = Logger.getLogger(PostCheckTurnRoute.class.getName());
+
+    private final PlayerLobby playerLobby;
+
+    public GetSpectatorExitGameRoute(PlayerLobby playerLobby) {
+        this.playerLobby = playerLobby;
+
+        LOG.fine("GetSpectatorExitGameRoute is initiated.");
+    }
+
     @Override
     public Object handle(Request request, Response response) throws Exception {
+        /*
+        TODO:
+            1. Remove the gameID from the player attribute so they can be
+                challenged to a game.
+            2. Redirect back to the homepage.
+         */
+        final Session httpSession = request.session();
+        final Player player = httpSession.attribute("player");
+
+        player.setGameID(-1);
+
+        response.redirect(WebServer.HOME_URL);
+
         return null;
     }
 }
