887712365a6a8cd53de2e852ee9ffe9d17d19715
Trevor Cruz <tdc8319@rit.edu>
2021-07-18 23:47:09 -0400

    Nearly fully implemented the Spectating feature. Still need to get the page to refresh automatically and to get the board to not flip.

--- src/main/java/com/webcheckers/ui/PostSpectatorCheckTurnRoute.java
+++ src/main/java/com/webcheckers/ui/PostSpectatorCheckTurnRoute.java
@@ -1,12 +1,42 @@
 package com.webcheckers.ui;
 
+import com.google.gson.Gson;
+import com.webcheckers.appl.GameManager;
+import com.webcheckers.model.CheckersGame;
+import com.webcheckers.model.Player;
+import com.webcheckers.util.Message;
 import spark.Request;
 import spark.Response;
 import spark.Route;
+import spark.Session;
+
+import java.util.logging.Logger;
 
 public class PostSpectatorCheckTurnRoute implements Route {
+
+    private static final Logger LOG = Logger.getLogger(PostCheckTurnRoute.class.getName());
+
+    private final GameManager gameManager;
+    private final Gson gson;
+
+    public PostSpectatorCheckTurnRoute(GameManager gameManager, Gson gson) {
+        this.gameManager = gameManager;
+        this.gson = gson;
+
+        LOG.fine("PostSpectatorCheckTurnRoute is initiated.");
+    }
+
     @Override
     public Object handle(Request request, Response response) throws Exception {
-        return null;
+        final Session httpSession = request.session();
+        final Player player = httpSession.attribute("player");
+
+        CheckersGame game = gameManager.getGame(player.getGameID());
+        Message result = Message.info("false");
+        if (game.isPlayersTurn(player)) {
+            result = Message.info("true");
+        }
+
+        return this.gson.toJson(result);
     }
 }
