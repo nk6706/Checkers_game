37e3ed910c48efcf202c17f89dc180ad0f7cce75
William Tietjen <wft4166@rit.edu>
2021-07-24 22:19:52 -0400

    implemented functionality

--- src/main/java/com/webcheckers/ui/PostReplayNextRoute.java
+++ src/main/java/com/webcheckers/ui/PostReplayNextRoute.java
@@ -1,8 +1,6 @@
 package com.webcheckers.ui;
 
 import com.google.gson.Gson;
-import com.webcheckers.appl.GameManager;
-import com.webcheckers.model.CheckersGame;
 import com.webcheckers.model.Player;
 import com.webcheckers.util.Message;
 import spark.Request;
@@ -15,8 +13,10 @@ import java.util.logging.Logger;
 public class PostReplayNextRoute implements Route {
     private static final Logger LOG = Logger.getLogger(PostReplayNextRoute.class.getName());
 
+    private final Gson gson;
 
-    public PostReplayNextRoute() {
+    public PostReplayNextRoute(Gson gson) {
+        this.gson = gson;
         //
         LOG.config("PostReplayNextRoute is initialized.");
     }
@@ -26,6 +26,20 @@ public class PostReplayNextRoute implements Route {
         final Session httpSession = request.session();
         final Player player = httpSession.attribute("player");
 
-        return null;
+        /**final String queryParam = request.queryParams("gameID");
+        if(queryParam == null) {
+            response.redirect(WebServer.HOME_URL);
+        }
+        final int gameID = Integer.parseInt(queryParam);
+
+        Message result = Message.info("false");
+        if (this.replayManager.hasNext(player.getUsername(), gameID)) {
+            result = Message.info("true");
+        }*/
+
+        final int replayPosition = httpSession.attribute("replayPosition");
+        httpSession.attribute("replayPosition", replayPosition+1);
+
+        return this.gson.toJson(Message.info("true"));
     }
 }
--- src/main/java/com/webcheckers/ui/PostReplayPreviousRoute.java
+++ src/main/java/com/webcheckers/ui/PostReplayPreviousRoute.java
@@ -1,8 +1,6 @@
 package com.webcheckers.ui;
 
 import com.google.gson.Gson;
-import com.webcheckers.appl.GameManager;
-import com.webcheckers.model.CheckersGame;
 import com.webcheckers.model.Player;
 import com.webcheckers.util.Message;
 import spark.Request;
@@ -15,8 +13,10 @@ import java.util.logging.Logger;
 public class PostReplayPreviousRoute implements Route {
     private static final Logger LOG = Logger.getLogger(PostReplayPreviousRoute.class.getName());
 
+    private final Gson gson;
 
-    public PostReplayPreviousRoute() {
+    public PostReplayPreviousRoute(Gson gson) {
+        this.gson = gson;
         //
         LOG.config("PostReplayPreviousRoute is initialized.");
     }
@@ -26,6 +26,20 @@ public class PostReplayPreviousRoute implements Route {
         final Session httpSession = request.session();
         final Player player = httpSession.attribute("player");
 
-        return null;
+        /**final String queryParam = request.queryParams("gameID");
+        if(queryParam == null) {
+            response.redirect(WebServer.HOME_URL);
+        }
+        final int gameID = Integer.parseInt(queryParam);
+
+        Message result = Message.info("false");
+        if (this.replayManager.hasPrevious(player.getUsername(), gameID)) {
+            result = Message.info("true");
+        }*/
+
+        final int replayPosition = httpSession.attribute("replayPosition");
+        httpSession.attribute("replayPosition", replayPosition-1);
+
+        return this.gson.toJson(Message.info("true"));
     }
 }
